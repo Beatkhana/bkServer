ac79cd3c8365aa948cbe2032879c036ca76b8913
Dan Carruthers <dan@pop-os.localdomain>
2020-05-28 12:59:01 +1000

    maybe

--- server.ts
+++ server.ts
@@ -26,7 +26,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-const PORT = +process.env.port || 8080;
+const PORT = +process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log("Server now listening on "+PORT);
