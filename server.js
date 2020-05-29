89cd95459cb5aeba7f0c34c87b36bc8400d75b38
Dan Carruthers <dan@pop-os.localdomain>
2020-05-30 01:31:42 +1000

    maybe

---
+++ server.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var router = require('./router');
+var app = express_1.default();
+var path = require('path');
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+    if ('OPTIONS' === req.method) {
+        res.sendStatus(200);
+    }
+    else {
+        console.log(req.ip + " " + req.method + " " + req.url);
+        next();
+    }
+});
+app.use(express_1.default.json());
+app.use('/', router);
+app.use(express_1.default.static(path.join(__dirname, 'public')));
+console.log(path.join(__dirname, 'public/index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+var PORT = +process.env.PORT || 8080;
+app.listen(PORT, function () {
+    console.log("Server now listening on " + PORT);
+});
---
+++ tournaments.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.tournaments = void 0;
+var database_1 = require("./database");
+var tournaments = /** @class */ (function () {
+    function tournaments() {
+        // public con: mysql.Connection;
+        // public connected: boolean = false;
+        this.db = new database_1.database();
+        // setInterval(function () {
+        //     this.db.query('SELECT 1');
+        // }, 5000);
+    }
+    tournaments.prototype.getAll = function (callback) {
+        var data = [];
+        var result = this.db.query("SELECT * FROM tournaments", function (result) {
+            return callback(result);
+        });
+    };
+    tournaments.prototype.getActive = function (callback) {
+        var data = [];
+        var result = this.db.query("SELECT * FROM tournaments WHERE archived = 0", function (result) {
+            return callback(result);
+        });
+    };
+    tournaments.prototype.getTournament = function (id, callback) {
+        var data = [];
+        var result = this.db.query("SELECT * FROM tournaments WHERE id = " + id, function (result) {
+            return callback(result);
+        });
+    };
+    return tournaments;
+}());
+exports.tournaments = tournaments;
