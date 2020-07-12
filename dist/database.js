5ee7bf130302fff592fca8c66ca10dbdbed8868e
Dannypoke03 <dannypoke03@beatsaberleague.com>
2020-07-12 16:05:39 +1000

    dont ignore dist, need that for heroku :)

---
+++ dist/database.js
@@ -0,0 +1,74 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.database = void 0;
+var mysql_1 = __importDefault(require("mysql"));
+// const mysql = mysql;
+var database = /** @class */ (function () {
+    function database() {
+        this.connected = false;
+        var env = process.env.NODE_ENV || 'production';
+        // console.log(env);
+        if (env == 'production') {
+            this.con = mysql_1.default.createPool({
+                host: "us-cdbr-east-02.cleardb.com",
+                user: "bdaa6c4e2efd54",
+                password: "f84071f4",
+                database: "heroku_da687e9a34aa489",
+                connectionLimit: 10
+            });
+        }
+        else {
+            this.con = mysql_1.default.createPool({
+                host: "us-cdbr-east-05.cleardb.net",
+                user: "bf459e897fa39a",
+                password: "88303776",
+                database: "heroku_11100f74419df40",
+                connectionLimit: 10
+            });
+        }
+        // this.con = mysql.createPool({
+        //     // host: "us-cdbr-east-05.cleardb.net",
+        //     // user: "bf459e897fa39a",
+        //     // password: "88303776",
+        //     // database: "heroku_11100f74419df40",
+        //     connectionLimit: 10,
+        //     // host: "sql230.main-hosting.eu",
+        //     host: "213.190.6.106",
+        //     user: "u826845424_beatKhanaTest",
+        //     password: "Vl0M6MbwGFyj",
+        //     database: "u826845424_beatKhanaTest"
+        // });
+    }
+    database.prototype.query = function (sql, callback) {
+        this.con.getConnection(function (err, connection) {
+            if (err)
+                throw err;
+            var query = connection.query(sql, function (error, results, fields) {
+                var result = results;
+                err = error;
+                connection.release();
+                // if (error) throw error;
+                return callback(err, result);
+            });
+        });
+    };
+    database.prototype.preparedQuery = function (sql, params, callback) {
+        this.con.getConnection(function (err, connection) {
+            if (err)
+                throw err;
+            var result;
+            var query = connection.query(sql, params, function (error, results, fields) {
+                result = results;
+                connection.release();
+                // console.log(query.sql);
+                // if (error) throw error;
+                return callback(error, result);
+            });
+        });
+    };
+    return database;
+}());
+exports.database = database;
---
+++ dist/userAuth.js
@@ -0,0 +1,138 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userAuth = void 0;
+var database_1 = require("./database");
+var fetch = require('node-fetch');
+var FormData = require('form-data');
+var request = require('request');
+var userAuth = /** @class */ (function () {
+    function userAuth() {
+        this.user = {};
+        this.db = new database_1.database();
+    }
+    userAuth.prototype.sendCode = function (code, callback) {
+        var _this = this;
+        var data = new FormData();
+        // beatkhana
+        data.append('client_id', '721696709331386398');
+        data.append('client_secret', 'LdOyEZhrU6uW_5yBAn7f8g2nvTJ_13Y6');
+        data.append('grant_type', 'authorization_code');
+        var env = process.env.NODE_ENV || 'production';
+        var redirect = "";
+        if (env == 'production') {
+            redirect = 'https://beatkhana.com/api/discordAuth';
+        }
+        else {
+            redirect = 'http://localhost:4200/api/discordAuth';
+        }
+        // console.log(redirect)
+        // console.log(env)
+        data.append('redirect_uri', redirect);
+        data.append('scope', 'identify');
+        data.append('code', code);
+        fetch('https://discordapp.com/api/oauth2/token', {
+            method: 'POST',
+            body: data,
+        })
+            .then(function (discordRes) { return discordRes.json(); })
+            .then(function (info) {
+            return info;
+        })
+            .then(function (info) { return fetch('https://discordapp.com/api/users/@me', {
+            headers: {
+                authorization: info.token_type + " " + info.access_token,
+            },
+        }); })
+            .then(function (userRes) { return userRes.json(); })
+            .then(function (data) {
+            _this.checkuser(data.id, function (userRes, newUser) {
+                callback(userRes, newUser);
+            });
+        })
+            .catch(function (error) {
+            console.error('Error:', error);
+        });
+    };
+    userAuth.prototype.checkuser = function (discordId, callback) {
+        if (discordId) {
+            var res = this.db.query("SELECT GROUP_CONCAT(DISTINCT ra.roleId SEPARATOR ', ') as roleIds, users.*, GROUP_CONCAT(DISTINCT r.roleName SEPARATOR ', ') as roleNames\n            FROM users\n            LEFT JOIN roleassignment ra ON ra.userId = users.discordId\n            LEFT JOIN roles r ON r.roleId = ra.roleId\n            WHERE users.discordId = " + discordId + "\n            GROUP BY users.discordId", function (err, result) {
+                if (result.length > 0) {
+                    // console.log(result);
+                    result[0].discordId = discordId.toString();
+                    if (result[0].roleNames != null) {
+                        result[0].roleIds = result[0].roleIds.split(', ');
+                        result[0].roleNames = result[0].roleNames.split(', ');
+                    }
+                    else {
+                        result[0].roleIds = [];
+                        result[0].roleNames = [];
+                    }
+                    callback(result);
+                }
+                else {
+                    result = [{
+                            discordId: discordId.toString()
+                        }];
+                    callback(result, true);
+                }
+            });
+        }
+    };
+    userAuth.prototype.newUser = function (data, callback) {
+        var _this = this;
+        // console.log(data);
+        this.getSSData(data.links.scoreSaber.split('u/')[1], function (ssData) {
+            var user = {
+                discordId: data.discordId,
+                ssId: ssData.playerInfo.playerId,
+                name: ssData.playerInfo.playerName,
+                twitchName: data.links.twitch.split('twitch.tv/')[1],
+                avatar: ssData.playerInfo.avatar,
+                globalRank: ssData.playerInfo.rank,
+                localRank: ssData.playerInfo.countryRank,
+                country: ssData.playerInfo.country
+            };
+            // console.log(user);
+            var result = _this.db.preparedQuery("INSERT INTO users SET ?", [user], function (err, result) {
+                // console.log(result);
+                // console.log(err);
+                var loggedUser = user;
+                loggedUser.roleIds = [];
+                loggedUser.roleNames = [];
+                return callback([user]);
+            });
+        });
+    };
+    userAuth.prototype.getSSData = function (id, callback) {
+        // console.log(`https://new.scoresaber.com/api/player/${id}/basic`);
+        // https.get(`https://new.scoresaber.com/api/player/${id}/basic`, (resp) => {
+        //     let data = '';
+        //     resp.on('end', () => {
+        //         console.log(JSON.parse(data).explanation);
+        //     });
+        // }).on("error", (err) => {
+        //     console.log("Error: " + err.message);
+        // });
+        request("https://new.scoresaber.com/api/player/" + id + "/basic", { json: true }, function (err, res, body) {
+            if (err) {
+                return console.log(err);
+            }
+            // console.log(body.url);
+            // console.log(body);
+            callback(body);
+        });
+    };
+    userAuth.prototype.getUser = function () {
+        if (this.user != {}) {
+            return this.user;
+        }
+        else {
+            return { 'loggedIn': false };
+        }
+    };
+    userAuth.prototype.logOut = function () {
+        this.user = {};
+    };
+    return userAuth;
+}());
+exports.userAuth = userAuth;
