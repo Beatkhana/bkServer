bc93efa54cb3db91217cbbbf6da23ec68efe7d29
Dannypoke03 <dannypoke03@beatsaberleague.com>
2021-02-13 22:12:34 +1100

    update frontend to support backend updates and other small fixes

--- controllers/debugLogger.controller.ts
+++ controllers/debugLogger.controller.ts
@@ -9,7 +9,7 @@ export class debugLogger {
         const debug = console.debug;
 
         console.debug = function () {
-            let args: [any, ...any[]] = ['\x1b[36m[DEBUG] %s\x1b[0m', ...Array.from(arguments)];
+            let args: [any, ...any[]] = [`\x1b[36m[${getTime()}][DEBUG] %s\x1b[0m`, ...Array.from(arguments)];
             if (env != 'producttion') {
                 debug.apply(console, args);
             }
@@ -18,7 +18,7 @@ export class debugLogger {
         const error = console.error;
 
         console.error = function () {
-            let args: [any, ...any[]] = ['\x1b[31m[ERROR] %s\x1b[0m', ...Array.from(arguments)];
+            let args: [any, ...any[]] = [`\x1b[31m[${getTime()}][ERROR] %s\x1b[0m`, ...Array.from(arguments)];
             error.apply(console, args);
         };
 
@@ -32,9 +32,13 @@ export class debugLogger {
         const info = console.info;
 
         console.info = function () {
-            let args: [any, ...any[]] = ['\x1b[90m[INFO] %s\x1b[0m', ...Array.from(arguments)];
+            let args: [any, ...any[]] = [`\x1b[90m[${getTime()}][INFO] %s\x1b[0m`, ...Array.from(arguments)];
             info.apply(console, args);
         };
     }
+}
 
+function getTime() {
+    var dt = new Date();
+    return `${(dt.getMonth() + 1).toString().padStart(2, '0')}/${dt.getDate().toString().padStart(2, '0')}/${dt.getFullYear().toString().padStart(4, '0')} ${dt.getHours().toString().padStart(2, '0')}:${dt.getMinutes().toString().padStart(2, '0')}:${dt.getSeconds().toString().padStart(2, '0')}`;
 }
