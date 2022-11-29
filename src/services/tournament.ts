7a2feda76e031c8f8bab6a9303cd2425a62af8ca
Dannypoke03 <daniel.carruthers@outlook.com>
2022-11-30 00:25:29 +1100

    tournament listings to use tournament service

--- src/services/tournament.ts
+++ src/services/tournament.ts
@@ -1,6 +1,56 @@
 import { prisma } from "./database";
 
 export class TournamentService {
+    public static async getTournaments({ auth, userId, mini = false, archived = false }: { auth: boolean; userId: string; mini?: boolean; archived?: boolean }) {
+        const tournaments = await prisma.tournaments.findMany({
+            include: {
+                tournament_settings: true
+            },
+            where: {
+                archived: false,
+                is_mini: +mini,
+                ...(!auth && {
+                    OR: [
+                        {
+                            tournament_settings: {
+                                public: 1
+                            }
+                        },
+                        {
+                            owner: userId
+                        },
+                        {
+                            tournament_role_assignment: {
+                                some: {
+                                    user_id: userId
+                                }
+                            }
+                        }
+                    ]
+                })
+            }
+        });
+        return tournaments.map(x => {
+            return {
+                tournamentId: x.id.toString(),
+                name: x.name,
+                image: x.image,
+                startDate: x.date,
+                endDate: x.endDate,
+                discord: x.discord,
+                twitchLink: x.twitchLink,
+                prize: x.prize,
+                info: x.info,
+                owner: x.owner,
+                archived: x.archived,
+                first: x.first,
+                second: x.second,
+                third: x.third,
+                public: x.tournament_settings.public
+            };
+        });
+    }
+
     public static async getTournament(tourneyId: number) {
         const tourney = await prisma.tournaments.findFirst({
             where: {
